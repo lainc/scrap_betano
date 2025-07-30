e3d5b87540cf44524db8a9a43d58fca267c98989
lainc <124663+lainc@users.noreply.github.com>
2025-07-30 20:37:23 -0300

    add 2 button clicks

---
+++ scrap.py
@@ -0,0 +1,61 @@
+from time import sleep
+from selenium.webdriver import Firefox, FirefoxOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+# from bs4 import BeautifulSoup
+# from pathlib import Path
+
+
+html_header = "<html>\n<head>\n\t<title>missing images</title>\n<head>\n<body>\n"
+html_ender  = "</body>\n</html>\n"
+url = r'https://www.betano.bet.br'
+
+estimated_fetch_time = 7
+def get_driver():
+    options = FirefoxOptions()
+    # options.add_argument("--headless=new")
+    driver = Firefox(options=options)
+    driver.implicitly_wait(estimated_fetch_time)
+    return driver
+
+# <button id="onetrust-accept-btn-handler">SIM, EU ACEITO</button>
+# <button class="button-class tw-border-n tw-border-solid tw-bg-n-22-licorice tw-border-n-28-cloud-burst tw-text-white-snow tw-uppercase tw-font-bold tw-p-nm tw-rounded-s tw-w-full" data-qa="age-verification-modal-ok-button"><span>Sim</span></button>
+
+def main():
+    driver = get_driver()
+    driver.get(url)
+    # wait = WebDriverWait(driver, timeout=6)
+    errors = [NoSuchElementException]
+    wait = WebDriverWait(driver, timeout=6, poll_frequency=.2, ignored_exceptions=errors)
+
+    # sim no aceitar cookies
+    ok_btn_id = "onetrust-accept-btn-handler"
+    acc_cookies = driver.find_element(By.ID, ok_btn_id)
+    acc_cookies.click()
+
+    # sim no "acima de 18"
+    age_ver = driver.find_element(By.ID, "age-verification-modal")
+    wait.until(lambda _ : age_ver.is_displayed())
+    over_18_btns = age_ver.find_elements(By.TAG_NAME, "button")
+    yes_btn = over_18_btns[1]
+    yes_btn.click()
+
+    # fechar telinha de registrar
+    # reg_pane_class = "modal-dialog modal-body-class"
+    # reg_pane_class = "landing-page-modal__primary-container"
+    # reg_pane_class = "landing-page-modal__image"
+    # reg_pane_class = "sb-modal__body"
+    # reg_pane = driver.find_element(By.CLASS_NAME, reg_pane_class)
+    reg_pane_id = "landing-page-modal"
+    reg_pane = driver.find_element(By.ID, reg_pane_id)
+    wait.until(EC.presence_of_element_located((By.ID, reg_pane_id)))
+
+    x_btn_id = "button-close sb-modal__close__btn modal-close-default"
+    x_btn = reg_pane.find_element(By.CLASS_NAME, x_btn_id)
+    x_btn.click()
+
+
+if __name__ == '__main__':
+    main()
